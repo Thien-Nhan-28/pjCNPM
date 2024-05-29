82617c55c16a1a5f51be63c08b534831d752fa6c
Thien-Nhan-28 <21130461@st.hcmuaf.edu.vn>
2024-05-29 14:16:00 +0700

    weather for day

---
+++ ProjectCNPM/app/src/main/java/com/example/weatherapp/data/LiveDataEvent.kt
@@ -0,0 +1,16 @@
+package com.example.weatherapp.data
+
+data class LiveDataEvent<out T>(private val content: T){
+
+    private var hasBeenHandled = false
+
+    fun getContentIfNotHandle(): T? {
+        return if (hasBeenHandled){
+            null
+        }else{
+            hasBeenHandled = true
+            content
+        }
+    }
+
+}
---
+++ ProjectCNPM/app/src/main/java/com/example/weatherapp/fragments/location/LocationsApdapter.kt
@@ -0,0 +1,50 @@
+package com.example.weatherapp.fragments.location
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.weatherapp.data.RemoteLocation
+import com.example.weatherapp.databinding.ItemContainerLocationBinding
+
+class LocationsApdapter(
+    private val onLocationClicked: (RemoteLocation) -> Unit
+) : RecyclerView.Adapter<LocationsApdapter.LocationViewHolder>() {
+
+    private val locations = mutableListOf<RemoteLocation>()
+
+    fun setData(data: List<RemoteLocation>) {
+        locations.clear()
+        locations.addAll(data)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LocationViewHolder {
+        return LocationViewHolder(
+            ItemContainerLocationBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: LocationViewHolder, position: Int) {
+        holder.bind(remoteLocation = locations[position])
+    }
+
+    override fun getItemCount(): Int {
+        return locations.size
+    }
+
+    inner class LocationViewHolder(
+        private val binding: ItemContainerLocationBinding
+    ) : RecyclerView.ViewHolder(binding.root){
+        fun bind(remoteLocation: RemoteLocation){
+            with(remoteLocation){
+                val location = "$name, $region, $country"
+                binding.textRemoteLocation.text = location
+                binding.root.setOnClickListener { onLocationClicked(remoteLocation) }
+            }
+        }
+    }
+}
